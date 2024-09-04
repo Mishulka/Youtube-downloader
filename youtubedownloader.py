73f26f2d77a1581215fee0d8c296eec18be0b3df
Mishulka <148446221+Mishulka@users.noreply.github.com>
2024-09-04 14:28:01 +0300

    Add main file
    yea

---
+++ youtubedownloader.py
@@ -0,0 +1,24 @@
+from pytube import YouTube
+
+
+def video_downloader(video_url):
+   
+    my_video = YouTube(video_url)
+    
+    my_video.streams.get_highest_resolution().download()
+    
+    return my_video.title
+
+try:
+    
+    youtube_link = input('Enter the YouTube link:')
+    print(f'Downloading your Video, please wait.......')
+    
+    video = video_downloader(youtube_link)
+    
+    print(f'"{video}" downloaded succussfully!!')
+
+except:
+    print(f'Failed to download video\nThe '\
+          'following might be the causes\n->No internet '\
+          'connection\n->Invalid video link')
